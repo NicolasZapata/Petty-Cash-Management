01c9d351d895f26bbead799edbbf836af6163115
NicolasZapata <35777697+NicolasZapata@users.noreply.github.com>
2023-12-28 12:22:21 -0500

    :construction: [Initial] Petty Cash Management

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+{
+  'name': 'Petty Cash Management',
+  'version': '1.0',
+  'description': 'Petty Cash Management',
+  'summary': '',
+  'author': 'Grupo Quanam Colombia SAS',
+  'website': 'grupoquanam.com.co',
+  'license': 'LGPL-3',
+  'category': '',
+  'depends': [
+    'hr',
+    'hr_expense',
+    'account'
+  ],
+  'data': [
+    'views/petty_cash_view.xml',
+    'security/ir.model.access.csv'
+  ],
+  # 'demo': [
+  #   ''
+  # ],
+  'auto_install': False,
+  'application': False,
+  'assets': {
+    
+  }
+}
