dbd79374876c79901eeb7cb80ecd0fa1b2f158aa
Leonardgoz <leonard.unal@gmail.com>
2023-12-29 13:55:30 -0500

    [Update] Update petty_cash.py fields One2Many

--- models/petty_cash.py
+++ models/petty_cash.py
@@ -13,11 +13,18 @@ class PettyCashManagement(models.Model):
       date_closed = fields.Date(string='date_closed')
       date_opened = fields.Date(string='date opened')
       refund_account_id = fields.Many2one('account.account', string='refund_account')
-      expense_to_report = fields.Float(string='Expense to report')
+      expense_ids = fields.One2many('hr.expense','petty_cash_management_id', string='Expenses')
+      expense_to_report = fields.Float(string='Expense to Report', compute='_compute_expense_to_report', readonly='True', store='True')
       validation_expenses = fields.Float(string='Validation Expenses')
       expenses_to_reimburse = fields.Float(string='Expenses to reimburse')
       cash_on_hand = fields.Float(string='Cash on hand')
       total = fields.Float(string='total', compute="total_cash_amount", store=True)
+
+      @api.depends('validation_expenses', 'expenses_to_reimburse')
+      def _compute_expense_to_report(self):
+            for record in self:
+                  draft_expenses = (record.expense_ids.filtered(lambda expense: expense.state == 'draft'))
+                  record.expense_to_report = sum(draft_expenses.mapped('total_amount'))
 
       @api.depends('expense_to_report', 'validation_expenses')
       def total_cash_amount(self):
