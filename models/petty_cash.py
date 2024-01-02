8f50ddea48fbc9a6cd2d0589543bd8f71cc3b565
NicolasZapata <35777697+NicolasZapata@users.noreply.github.com>
2024-01-02 18:31:39 -0500

    :bug: [Development] Validation Draft, Reported and Approved in Petty Cash

--- models/petty_cash.py
+++ models/petty_cash.py
@@ -5,7 +5,7 @@ class PettyCashManagement(models.Model):
       _name = 'petty.cash.management'
       _description = 'PettyCash'
       
-      name=fields.Char(string='Petty Cash Management')
+      name = fields.Char(string='Petty Cash Management')
       responsable_id = fields.Many2one('hr.employee', string='responsable')
       cash_amount = fields.Float(string='cash_amount')
       cash_notes = fields.Html(string='cash_notes')
@@ -24,19 +24,33 @@ class PettyCashManagement(models.Model):
             ('closed', 'closed')
       ], string='Petty Cash States')
 
-      @api.depends('validation_expenses', 'expenses_to_reimburse')
-      @api.onchange('cash_amount')
-      def _compute_expense_to_report(self):
-            for record in self:
-                  draft_expenses = (record.expense_ids.filtered(lambda expense: expense.state == 'draft'))
-                  record.expense_to_report = sum(draft_expenses.mapped('total_amount'))
-
       @api.depends('expense_to_report', 'validation_expenses')
-      @api.onchange('cash_amount')
+      @api.onchange('cash_on_hand')
       def _total_cash_amount(self):
             self.cash_on_hand = self.cash_amount - self.expense_to_report \
             - self.validation_expenses - self.expense_to_report \
             - self.expenses_to_reimburse
 
-      def _open_my_expenses():
-            return {'type': 'ir.actions.act_window_close'}
+      @api.depends('validation_expenses', 'expenses_to_reimburse')
+      def _compute_expense_to_report(self):
+            for record in self:
+                  draft_expenses = (record.expense_ids.filtered(
+                        lambda expense: expense.state == 'draft'
+                  ))
+                  record.expense_to_report = sum(draft_expenses.mapped('total_amount'))
+
+      @api.depends('validation_expenses', 'expenses_to_reimburse')
+      def _draft_expense_to_report(self):
+            for record in self:
+                  draft_expenses = (record.expense_ids.filtered(
+                        lambda expense: expense.state == 'reported'
+                  ))
+                  record.expense_to_report = sum(draft_expenses.mapped('total_amount'))
+
+      @api.depends('validation_expenses', 'expenses_to_reimburse')
+      def _draft_expense_to_report(self):
+            for record in self:
+                  draft_expenses = (record.expense_ids.filtered(
+                        lambda expense: expense.state == 'approved'
+                  ))
+                  record.expense_to_report = sum(draft_expenses.mapped('total_amount'))                 
