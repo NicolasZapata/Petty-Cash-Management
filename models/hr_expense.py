897895ce3e6c4633f57a41515a1248cb77e2d8f8
NicolasZapata <35777697+NicolasZapata@users.noreply.github.com>
2024-01-05 14:45:43 -0500

    :white_check_mark: [Update] Report Smart Button

--- models/hr_expense.py
+++ models/hr_expense.py
@@ -1,18 +1,24 @@
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
+
 class HrExpense(models.Model):
-  _inherit = 'hr.expense'
-  _description = 'HrExpense'
+    _inherit = "hr.expense"
+    _description = "HrExpense"
 
-  petty_cash_management_id = fields.Many2one(
-    'petty.cash.management', 
-    string='Petty Cash Management', 
-    required='True'
-  )
+    petty_cash_management_id = fields.Many2one(
+        "petty.cash.management", string="Petty Cash Management", required="True"
+    )
 
-  @api.constrains('petty_cash_management_id')
-  def _ckeck_petty_cash_id(self):
-    for expense in self:
-      if expense.report_id and expense.petty_cash_management_id.expense_ids.filtered(lambda x: x.id != expense.id):
-          raise ValidationError("You can´t select more than two petty cashes")
+    # TODO: Ajust for filter two petty cashes and get an validation error
+    @api.constrains("petty_cash_management_id")
+    def action_ckeck_petty_cash_id(self):
+        for expense in self:
+            if (
+                expense
+                and expense.report_id.petty_cash_management_id
+                != expense.petty_cash_management_id
+            ):
+                raise ValidationError(
+                    "You cant select more than two petty cashes"
+                )
--- models/petty_cash.py
+++ models/petty_cash.py
@@ -1,98 +1,131 @@
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError, UserError
 
+
 class PettyCashManagement(models.Model):
-      _name = 'petty.cash.management'
-      _description = 'PettyCash'
-      _inherit = ['mail.thread', 'mail.activity.mixin']
+    _name = "petty.cash.management"
+    _description = "PettyCash"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
 
-      name = fields.Char(string='Petty Cash Management')
-      responsable_id = fields.Many2one('hr.employee', string='responsable')
-      cash_amount = fields.Float(string='cash_amount', tracking=True)
-      cash_notes = fields.Html(string='cash_notes', tracking=True)
-      date_closed = fields.Date(string='date_closed', tracking=True)
-      date_opened = fields.Date(string='date opened', tracking=True)
-      refund_account_id = fields.Many2one('account.account', string='refund_account')
-      expense_ids = fields.One2many('hr.expense','petty_cash_management_id', string='Expenses')
-      expense_sheets_ids = fields.One2many('hr.expense.sheet','petty_cash_management_sheet_id', string='Expenses')
-      expense_to_report = fields.Float(string='Expense to Report', compute='_compute_expense_to_report', readonly='True', store='True')
-      expenses_to_approve = fields.Float(string='Expenses to validate', compute='_compute_expenses_to_approve', readonly='True', store='True')
-      expenses_to_reimburse = fields.Float(string='Expenses to reimburse', compute='_compute_expense_to_reimburse', readonly='True', store='True')
-      cash_on_hand = fields.Float(compute="_total_cash_amount", string='Cash on hand', tracking=True)
-      expenses_to_approve = fields.Float(string='Expense to Approve', compute='_compute_expenses_to_approve', readonly='True', store='True')
+    name = fields.Char(string="Petty Cash Management")
+    responsable_id = fields.Many2one("hr.employee", string="responsable")
+    cash_amount = fields.Float(string="cash_amount", tracking=True)
+    cash_notes = fields.Html(string="cash_notes", tracking=True)
+    date_closed = fields.Date(string="date_closed", tracking=True)
+    date_opened = fields.Date(string="date opened", tracking=True)
+    refund_account_id = fields.Many2one("account.account", string="refund_account")
+    expense_ids = fields.One2many(
+        "hr.expense", "petty_cash_management_id", string="Expenses"
+    )
+    expense_sheets_ids = fields.One2many(
+        "hr.expense.sheet", "petty_cash_management_sheet_id", string="Expenses"
+    )
+    expense_to_report = fields.Float(
+        string="Expense to Report",
+        compute="_compute_expense_to_report",
+        readonly="True",
+        store="True",
+    )
+    expenses_to_approve = fields.Float(
+        string="Expenses to validate",
+        compute="_compute_expenses_to_approve",
+        readonly="True",
+        store="True",
+    )
+    expenses_to_reimburse = fields.Float(
+        string="Expenses to reimburse",
+        compute="_compute_expense_to_reimburse",
+        readonly="True",
+        store="True",
+    )
+    cash_on_hand = fields.Float(
+        compute="_total_cash_amount", string="Cash on hand", tracking=True
+    )
+    expenses_to_approve = fields.Float(
+        string="Expense to Approve",
+        compute="_compute_expenses_to_approve",
+        readonly="True",
+        store="True",
+    )
 
-      petty_cash_states = fields.Selection([
-            ('draft', 'draft'),
-            ('open', 'open'),
-            ('closed', 'closed')
-      ], string='Petty Cash States', default="draft")
+    petty_cash_states = fields.Selection(
+        [
+            ("draft", "Draft"),
+            ("open", "Open Petty Cash"),
+            ("closed", "Close Petty Cash"),
+        ],
+        string="Petty Cash States",
+        default="draft",
+    )
 
-      # Buttons
-      def btnDraftPettyCash(self):
-            self.petty_cash_states = 'draft'
+    # Buttons
+    def btnDraftPettyCash(self):
+        self.petty_cash_states = "draft"
 
-      def btnOpenPettyCash(self):
-            self.petty_cash_states = 'open'
+    def btnOpenPettyCash(self):
+        self.petty_cash_states = "open"
 
-      def btnClosePettyCash(self):
-            self.petty_cash_states = 'closed'
+    def btnClosePettyCash(self):
+        self.petty_cash_states = "closed"
 
-      # Smart Buttons
-      def action_open_expenses_view(self):
-            self.ensure_one()
-            return {
-                  'name': _('Expenses'),
-                  'type': 'ir.actions.act_window',
-                  'view_mode': 'list,form',
-                  'res_model': 'hr.expense',
-                  'domain': [('id', 'in', self.expense_ids.mapped('id'))],
-            }
+    # Smart Buttons
+    def action_open_expenses_view(self):
+        self.ensure_one()
+        return {
+            "name": _("Expenses"),
+            "type": "ir.actions.act_window",
+            "view_mode": "list,form",
+            "res_model": "hr.expense",
+            "domain": [("id", "in", self.expense_ids.mapped("id"))],
+        }
 
-      def action_open_expenses_reports(self):
-            self.ensure_one()
-            action = self.env.ref('hr_expense.action_hr_expense_sheet_all').read()[0]
-            return action
+    def action_open_expenses_reports(self):
+        self.ensure_one()
+        return self.env.ref("hr_expense.action_hr_expense_sheet_all").read()[0]
 
-      def action_generate_expense_invoice(self):
-            self.ensure_one()
-            action = self.env.ref('hr_expense.action_report_hr_expense_sheet').read()[0]
-            return action
+    def action_generate_expense_invoice(self):
+        self.ensure_one()
+        return self.env.ref("hr_expense.action_report_hr_expense_sheet").read()[0]
 
-      # COMPUTE METHODS
-      #Expenses to report state is in draft
-      @api.depends('expense_ids', 'expense_ids.state', 'expense_ids.total_amount')
-      def _compute_expense_to_report(self):
-            for record in self:
-                  report_expenses = (record.expense_ids.filtered(
-                        lambda expense: expense.state == 'draft' \
-                        and expense.petty_cash_management_id == record
-                  ))
-                  record.expense_to_report = sum(report_expenses.mapped('total_amount'))
+    # COMPUTE METHODS
+    # Expenses to report state is in draft
+    @api.depends("expense_ids", "expense_ids.state", "expense_ids.total_amount")
+    def _compute_expense_to_report(self):
+        for record in self:
+            report_expenses = record.expense_ids.filtered(
+                lambda expense: expense.state == "draft"
+                and expense.petty_cash_management_id == record
+            )
+            record.expense_to_report = sum(report_expenses.mapped("total_amount"))
 
-      #Expenses to approve state is in reported
-      @api.depends('expense_ids', 'expense_ids.state', 'expense_ids.total_amount')
-      def _compute_expenses_to_approve(self):
-            for record in self:
-                  approve_expenses = (record.expense_ids.filtered(
-                        lambda expense: expense.state == 'reported' \
-                        and expense.petty_cash_management_id == record
-                  ))
-                  record.expenses_to_approve = sum(approve_expenses.mapped('total_amount'))
+    # Expenses to approve state is in reported
+    @api.depends("expense_ids", "expense_ids.state", "expense_ids.total_amount")
+    def _compute_expenses_to_approve(self):
+        for record in self:
+            approve_expenses = record.expense_ids.filtered(
+                lambda expense: expense.state == "reported"
+                and expense.petty_cash_management_id == record
+            )
+            record.expenses_to_approve = sum(approve_expenses.mapped("total_amount"))
 
-      #Expenses to reimburse state is in approved
-      @api.depends('expense_ids', 'expense_ids.state', 'expense_ids.total_amount')
-      def _compute_expense_to_reimburse(self):
-            for record in self:
-                  reimburse_expenses = (record.expense_ids.filtered(
-                        lambda expense: expense.state == 'approved' \
-                        and expense.petty_cash_management_id == record
-                  ))
-                  record.expenses_to_reimburse = sum(reimburse_expenses.mapped('total_amount'))
+    # Expenses to reimburse state is in approved
+    @api.depends("expense_ids", "expense_ids.state", "expense_ids.total_amount")
+    def _compute_expense_to_reimburse(self):
+        for record in self:
+            reimburse_expenses = record.expense_ids.filtered(
+                lambda expense: expense.state == "approved"
+                and expense.petty_cash_management_id == record
+            )
+            record.expenses_to_reimburse = sum(
+                reimburse_expenses.mapped("total_amount")
+            )
 
-      # Operation in Cash on hands
-      @api.depends('expense_ids', 'expense_ids.state', 'expense_ids.total_amount')
-      def _total_cash_amount(self):
-            self.cash_on_hand = (self.cash_amount \
-                                 - self.expense_to_report \
-                                 - self.expenses_to_approve \
-                                 - self.expenses_to_reimburse)
+    # Operation in Cash on hands
+    @api.depends("expense_ids", "expense_ids.state", "expense_ids.total_amount")
+    def _total_cash_amount(self):
+        self.cash_on_hand = (
+            self.cash_amount
+            - self.expense_to_report
+            - self.expenses_to_approve
+            - self.expenses_to_reimburse
+        )
